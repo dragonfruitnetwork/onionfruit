a7bbe308861f1c5e1d0665fbb334fb825e60f74a
Albie Spriddell <albion@dragonfruit.network>
2024-01-04 12:32:00 +0000

    add connect/disconnect/authenticate methods

--- DragonFruit.OnionFruit.Core/Process/TorControlPortClient.cs
+++ DragonFruit.OnionFruit.Core/Process/TorControlPortClient.cs
@@ -12,14 +12,34 @@ using System.Threading.Tasks;
 namespace DragonFruit.OnionFruit.Core.Process
 {
     public record TorControlServerMessage(int Status, string StatusMessage, string Reply, string Data);
+    public record TorControlServerMessage(int Status, string StatusMessage, string ReplyData);
 
     /// <summary>
-    /// Allows for control of a tor process via the control port
+    /// Allows for control of a Tor process via the exposed RPC interface (control port)
     /// </summary>
     public class TorControlPortClient(IPEndPoint endpoint) : IDisposable
     {
-        private readonly TcpClient _client = new(endpoint);
+        private readonly TcpClient _client = new(endpoint.AddressFamily);
         private readonly SemaphoreSlim _semaphore = new(1, 1);
+
+        /// <summary>
+        /// Connects to the control port as an asynchronous operation
+        /// </summary>
+        public Task ConnectAsync() => _client.ConnectAsync(endpoint);
+
+        /// <summary>
+        /// Disconnects from the control port
+        /// </summary>
+        public void Disconnect() => _client.Close();
+
+        /// <summary>
+        /// Authenticates the control port with the given password as an asynchronous operation
+        /// </summary>
+        /// <returns>The result of the authentication operation (250 OK or error code)</returns>
+        public Task<TorControlServerMessage> AuthenticateAsync(string password)
+        {
+            return SendAsync($"AUTHENTICATE \"{password}\"");
+        }
 
         /// <summary>
         /// Obtains exclusive access to the control port, sends a message and processes the response as an asynchronous operation
@@ -40,10 +60,8 @@ namespace DragonFruit.OnionFruit.Core.Process
                     await writer.WriteLineAsync(message).ConfigureAwait(false);
                 }
 
-                var replyLines = new StringBuilder();
-                var dataLines = new StringBuilder();
-
                 using var reader = new StreamReader(networkStream, leaveOpen: true);
+                var dataLines = new StringBuilder();
 
                 while (true)
                 {
@@ -58,14 +76,14 @@ namespace DragonFruit.OnionFruit.Core.Process
                     switch (responseLine[3])
                     {
                         case ' ': // statusCode followed by a space indicates the end of the message
-                            return new TorControlServerMessage(int.Parse(responseLine[..3]), responseLine[4..], replyLines.ToString(), dataLines.ToString());
+                            return new TorControlServerMessage(int.Parse(responseLine[..3]), responseLine[4..], dataLines.ToString());
 
                         case '-': // statusCode followed by a hyphen indicates a mid-reply line
-                            replyLines.AppendLine(responseLine[4..]);
+                            dataLines.AppendLine(responseLine[4..]);
                             break;
 
-                        case '+': // statusCode followed by a plus indicates the lines preceding this one are data lines
-                            replyLines.AppendLine(responseLine[4..]);
+                        case '+': // statusCode followed by a plus is a multiline response (keep reading until a line with a single period (.) is encountered)
+                            dataLines.AppendLine(responseLine[4..]);
 
                             while (true)
                             {
