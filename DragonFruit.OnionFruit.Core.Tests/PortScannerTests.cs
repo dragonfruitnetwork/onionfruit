be5cb11d79080aa0a82d4bd5e19992905c96c760
Albie Spriddell <albion@dragonfruit.network>
2024-01-05 11:40:27 +0000

    add port scanner tests

---
+++ DragonFruit.OnionFruit.Core.Tests/PortScannerTests.cs
@@ -0,0 +1,29 @@
+using System.Net;
+using System.Net.Sockets;
+using DragonFruit.OnionFruit.Core.Network;
+using Xunit;
+
+namespace DragonFruit.OnionFruit.Core.Tests
+{
+    public class PortScannerTests
+    {
+        [Fact]
+        public void TestPortScanner()
+        {
+            const int target = 9999;
+
+            var port = PortScanner.GetClosestFreePort(target);
+            Assert.NotNull(port);
+
+            // open a tcpserver to occupy the port
+            using var server = new TcpListener(IPAddress.Loopback, port.Value);
+            server.Start();
+
+            // try to find a new port
+            var newPort = PortScanner.GetClosestFreePort(target);
+            Assert.NotEqual(port, newPort);
+
+            server.Stop();
+        }
+    }
+}
