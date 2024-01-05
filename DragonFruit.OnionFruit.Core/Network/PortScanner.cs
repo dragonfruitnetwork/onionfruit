be550837ae9cb9e6ad77797cae4c524f2b2a0a59
Albie Spriddell <albion@dragonfruit.network>
2024-01-05 10:38:34 +0000

    add port scanning utility method

---
+++ DragonFruit.OnionFruit.Core/Network/PortScanner.cs
@@ -0,0 +1,34 @@
+﻿// OnionFruit Copyright DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under LGPL-3.0. Refer to the LICENCE file for more info
+
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+
+namespace DragonFruit.OnionFruit.Core.Network
+{
+    public static class PortScanner
+    {
+        /// <summary>
+        /// Produces a set of all active TCP ports on the local system
+        /// </summary>
+        public static IReadOnlySet<int> GetActiveTcpPorts()
+        {
+            var properties = IPGlobalProperties.GetIPGlobalProperties();
+            var connections = properties.GetActiveTcpConnections();
+
+            var ports = new HashSet<int>(connections.Length);
+
+            foreach (var connection in connections)
+            {
+                if (connection.State == TcpState.Closed)
+                {
+                    continue;
+                }
+
+                ports.Add(connection.LocalEndPoint.Port);
+            }
+
+            return ports;
+        }
+    }
+}
