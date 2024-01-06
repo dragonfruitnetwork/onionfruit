ed9ed64e167fc2fbe6d78412576891c506e579d3
Albie Spriddell <albion@dragonfruit.network>
2024-01-06 18:36:36 +0000

    add logging and fix bad file sharing handles

--- DragonFruit.OnionFruit.Core/TorProcess.cs
+++ DragonFruit.OnionFruit.Core/TorProcess.cs
@@ -5,19 +5,25 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using DragonFruit.OnionFruit.Core.Config;
+using Microsoft.Extensions.Logging;
 
 namespace DragonFruit.OnionFruit.Core
 {
-    public class TorProcess(string torPath)
+    public class TorProcess(string torPath, ILogger logger = null)
     {
         private Process _process;
-        private FileStream _tempConfigFile;
+        private string _tempConfigFile;
 
         private State _processState = State.Stopped;
         private int _bootstrapProgress;
+
+        // why is this not a standard property?
+        private static readonly Encoding UTF8NoBom = new UTF8Encoding(false);
 
         /// <summary>
         /// Gets the version of the running client.
@@ -69,24 +75,35 @@ namespace DragonFruit.OnionFruit.Core
         /// On exit, the file is deleted.
         /// </remarks>
         /// <param name="configEntries">The <see cref="TorrcConfigEntry"/> items to use when building the torrc file</param>
-        public async Task StartProcessAsync(IEnumerable<TorrcConfigEntry> configEntries)
+        public Task StartProcessWithConfig(params TorrcConfigEntry[] configEntries) => StartProcessWithConfig(configEntries.AsEnumerable());
+
+        /// <summary>
+        /// Starts the Tor process asynchronously using the given config entries
+        /// </summary>
+        /// <remarks>
+        /// This method writes a temporary config file and starts the process using that.
+        /// On exit, the file is deleted.
+        /// </remarks>
+        /// <param name="configEntries">The <see cref="TorrcConfigEntry"/> items to use when building the torrc file</param>
+        public async Task StartProcessWithConfig(IEnumerable<TorrcConfigEntry> configEntries)
         {
             if (_process != null || _tempConfigFile != null)
             {
                 throw new InvalidOperationException("Tor process is already running. It must be stopped before starting again");
             }
 
-            _tempConfigFile = new FileStream(Path.GetTempFileName(), FileMode.Create, FileAccess.Write, FileShare.Read, 4096, FileOptions.Asynchronous | FileOptions.DeleteOnClose);
+            _tempConfigFile = Path.GetTempFileName();
 
-            await using (var writer = new StreamWriter(_tempConfigFile, leaveOpen: true))
+            // it would have been nice to create this with DeleteOnClose set, but it doesn't work due to how file sharing works
+            await using (var writer = new StreamWriter(new FileStream(_tempConfigFile, FileMode.Truncate, FileAccess.Write, FileShare.None, 1024, FileOptions.Asynchronous), UTF8NoBom))
             {
                 foreach (var entry in configEntries)
                 {
-                    await writer.WriteLineAsync(entry.ToString()).ConfigureAwait(false);
+                    await entry.WriteAsync(writer).ConfigureAwait(false);
                 }
             }
 
-            StartProcessAsync(_tempConfigFile.Name);
+            StartProcess(_tempConfigFile);
         }
 
         /// <summary>
@@ -94,7 +111,7 @@ namespace DragonFruit.OnionFruit.Core
         /// </summary>
         /// <param name="configFile"></param>
         /// <exception cref="InvalidOperationException">The tor process was not cleaned up previously.</exception>
-        public void StartProcessAsync(string configFile)
+        public void StartProcess(string configFile)
         {
             if (_process != null)
             {
@@ -130,7 +147,7 @@ namespace DragonFruit.OnionFruit.Core
             _process.BeginOutputReadLine();
         }
 
-        private async Task StopProcessAsync()
+        public async Task StopProcessAsync()
         {
             if (_process == null)
             {
@@ -170,14 +187,25 @@ namespace DragonFruit.OnionFruit.Core
 
             if (_tempConfigFile != null)
             {
-                await _tempConfigFile.DisposeAsync().ConfigureAwait(false);
+                try
+                {
+                    File.Delete(_tempConfigFile);
+                }
+                catch (Exception e)
+                {
+                    logger?.Log(LogLevel.Information, e, "Failed to delete temporary config file ({name}): {msg}", Path.GetFileName(_tempConfigFile), e.Message);
+                }
             }
 
             _process = null;
             _tempConfigFile = null;
 
             Version = null;
-            ProcessState = State.Stopped;
+
+            if (ProcessState != State.Killed)
+            {
+                ProcessState = State.Stopped;
+            }
         }
 
         private void ProcessOutput(object sender, DataReceivedEventArgs e)
@@ -204,11 +232,12 @@ namespace DragonFruit.OnionFruit.Core
 
             if (!logOutput.Success)
             {
-                // todo log raw output
+                logger?.Log(LogLevel.Debug, "[Tor]: {output}", e.Data);
                 return;
             }
 
             // todo log message using it's own priority
+            logger?.Log(LogLevel.Information, "[Tor]: {output}", logOutput.Groups["message"].Value);
 
             // check if it's a bootstrap message
             var bootstrapOutput = TorProcessOutputRegex.BootstrapLogOutput().Match(logOutput.Groups["message"].Value);
