4c91df07502b467e7983361f081c558a7f639dbc
Albie Spriddell <albion@dragonfruit.network>
2024-01-08 15:08:41 +0000

    add windows runtime for gui project

---
+++ DragonFruit.OnionFruit.Windows/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Avalonia;
+using Avalonia.ReactiveUI;
+using DragonFruit.OnionFruit.ViewModels;
+using DragonFruit.OnionFruit.Windows.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace DragonFruit.OnionFruit.Windows;
+
+public static class Program
+{
+    // Initialization code. Don't use any Avalonia, third-party APIs or any
+    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+    // yet and stuff might break.
+    [STAThread]
+    public static void Main(string[] args)
+    {
+        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+    }
+
+    // Avalonia configuration, don't remove; also used by visual designer.
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure(() => new App(BuildHost()))
+            .UsePlatformDetect()
+            .WithInterFont()
+            .LogToTrace()
+            .UseReactiveUI();
+
+    private static IHost BuildHost() =>
+        Host.CreateDefaultBuilder()
+            .ConfigureServices((context, services) =>
+            {
+                // register view models
+                services.AddTransient<MainWindowViewModel, Win32MainWindowViewModel>();
+            })
+            .Build();
+}
---
+++ DragonFruit.OnionFruit.Windows/ViewModels/Win32MainWindowViewModel.cs
@@ -0,0 +1,29 @@
+﻿// OnionFruit Copyright DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under LGPL-3.0. Refer to the LICENCE file for more info
+
+using System;
+using Avalonia;
+using Avalonia.Platform;
+using DragonFruit.OnionFruit.ViewModels;
+using DragonFruit.OnionFruit.ViewModels.Interfaces;
+
+namespace DragonFruit.OnionFruit.Windows.ViewModels
+{
+    public class Win32MainWindowViewModel : MainWindowViewModel, IHasCustomStartupPosition
+    {
+        PixelPoint IHasCustomStartupPosition.GetInitialPosition(Screen screen, Size clientSize)
+        {
+            var screenSize = screen.WorkingArea.Size;
+
+            var padding = (int)(10 * screen.Scaling);
+            var windowSize = PixelSize.FromSize(clientSize, screen.Scaling);
+
+            // account for when scaling != 1: the taskbar is scaled, but the workingarea doesn't account for that.
+            var scaledOffsetX = (int)Math.Ceiling((screen.Bounds.Width - screenSize.Width) * (1 / screen.Scaling));
+            var scaledOffsetY = (int)Math.Ceiling((screen.Bounds.Height - screenSize.Height) * (1 / screen.Scaling));
+
+            // apply padding to x axis only - the title bar is merged with the window so it gets its own padding
+            return new PixelPoint(screenSize.Width - windowSize.Width - scaledOffsetX - padding, screenSize.Height - windowSize.Height - scaledOffsetY + padding);
+        }
+    }
+}
