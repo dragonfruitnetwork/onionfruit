6ccc2d21bded1fdffd7c20ec6183ee8bc4716907
Albie Spriddell <albion@dragonfruit.network>
2024-01-21 11:47:18 +0000

    add onion.db download request

---
+++ DragonFruit.OnionFruit/Services/OnionDb/OnionDbDownloadRequest.cs
@@ -0,0 +1,20 @@
+// OnionFruit Copyright DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under LGPL-3.0. Refer to the LICENCE file for more info
+
+using System;
+using DragonFruit.Data;
+using DragonFruit.Data.Requests;
+
+namespace DragonFruit.OnionFruit.Services.OnionDb
+{
+    /// <summary>
+    /// Represents a request to download the latest onion.db file
+    /// </summary>
+    public partial class OnionDbDownloadRequest(DateTimeOffset? previousDatabaseVersion) : ApiRequest
+    {
+        public override string RequestPath => "https://onionfruit-api.dragonfruit.network/assets/onion.db";
+
+        [RequestParameter(ParameterType.Header, "If-Modified-Since")]
+        protected string PreviousDatabaseVersionString => previousDatabaseVersion?.ToString("R");
+    }
+}
