95e35bd0078012773e250611c4cfdc7ce181ad33
Albie Spriddell <albion@dragonfruit.network>
2024-05-22 20:50:13 +0100

    add basic settings store

---
+++ DragonFruit.OnionFruit/Configuration/OnionFruitSettingsStore.cs
@@ -0,0 +1,83 @@
+﻿// OnionFruit Copyright DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under LGPL-3.0. Refer to the LICENCE file for more info
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Threading.Tasks;
+using DragonFruit.OnionFruit.Database;
+using CodedOutputStream = Google.Protobuf.CodedOutputStream;
+
+namespace DragonFruit.OnionFruit.Configuration
+{
+    public class OnionFruitSettingsStore : SettingsStore<OnionFruitSetting>
+    {
+        private readonly List<Action<OnionFruitConfigFile>> _valueApplicators = [];
+        private OnionFruitConfigFile _configFile;
+
+        private string ConfigurationFile => Path.Combine(App.StoragePath, "onionfruit.cfg");
+
+        public OnionFruitSettingsStore()
+        {
+            RegisterOption(OnionFruitSetting.TorEntryCountryCode, IOnionDatabase.TorCountryCode, static c => c.EntryCountryCode, static (c, val) => c.EntryCountryCode = val);
+            RegisterOption(OnionFruitSetting.TorExitCountryCode, IOnionDatabase.TorCountryCode, static c => c.ExitCountryCode, static (c, val) => c.ExitCountryCode = val);
+        }
+
+        protected override async Task LoadConfiguration()
+        {
+            if (File.Exists(ConfigurationFile))
+            {
+                var configBytes = await File.ReadAllBytesAsync(ConfigurationFile).ConfigureAwait(false);
+                _configFile = OnionFruitConfigFile.Parser.ParseFrom(configBytes);
+            }
+            else
+            {
+                _configFile = new OnionFruitConfigFile();
+            }
+
+            UpdateRegisteredValues();
+        }
+
+        protected override Task SaveConfiguration()
+        {
+            using var codedOutputStream = new CodedOutputStream(File.Create(ConfigurationFile));
+
+            _configFile.WriteTo(codedOutputStream);
+            return Task.CompletedTask;
+        }
+
+        protected void RegisterOption<T>(OnionFruitSetting key, T defaultValue, Func<OnionFruitConfigFile, T> getter, Action<OnionFruitConfigFile, T> setter)
+        {
+            if (ConfigStore.ContainsKey(key))
+            {
+                throw new ArgumentException($"Key {key} already exists in the configuration store");
+            }
+
+            var subject = new BehaviorSubject<T>(defaultValue);
+
+            ConfigStore.Add(key, subject);
+            Subscriptions.Add(subject.DistinctUntilChanged().Subscribe(value => setter.Invoke(_configFile, value)));
+
+            _valueApplicators.Add(c => subject.OnNext(getter.Invoke(c)));
+        }
+
+        /// <summary>
+        /// Invokes all getters against the current configuration object to update stored values
+        /// </summary>
+        private void UpdateRegisteredValues()
+        {
+            foreach (var applicator in _valueApplicators)
+            {
+                applicator.Invoke(_configFile);
+            }
+        }
+    }
+
+    public enum OnionFruitSetting
+    {
+        TorEntryCountryCode,
+        TorExitCountryCode
+    }
+}
---
+++ DragonFruit.OnionFruit/Configuration/SettingsStore.cs
@@ -0,0 +1,76 @@
+﻿// OnionFruit Copyright DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under LGPL-3.0. Refer to the LICENCE file for more info
+
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Reactive.Subjects;
+using System.Threading.Tasks;
+
+namespace DragonFruit.OnionFruit.Configuration
+{
+    public abstract class SettingsStore<TKey> : IDisposable where TKey : Enum
+    {
+        protected readonly IDictionary<TKey, object> ConfigStore = new ConcurrentDictionary<TKey, object>();
+        protected readonly CompositeDisposable Subscriptions = new();
+
+        /// <summary>
+        /// Saves the current configuration to persistent storage
+        /// </summary>
+        protected abstract Task SaveConfiguration();
+
+        /// <summary>
+        /// Loads the configuration from storage
+        /// </summary>
+        /// <returns></returns>
+        protected abstract Task LoadConfiguration();
+
+        /// <summary>
+        /// Gets the current value of a specified configuration key
+        /// </summary>
+        public TValue GetValue<TValue>(TKey key) => GetRootSubject<TValue>(key).Value;
+
+        /// <summary>
+        /// Gets a source for observing changes to a configuration value
+        /// </summary>
+        /// <param name="key">The key to get an observable notification stream for</param>
+        public IObservable<TValue> GetObservableValue<TValue>(TKey key) => GetRootSubject<TValue>(key);
+
+        /// <summary>
+        /// Sets a configuration value, informing observers of the change
+        /// </summary>
+        public void SetValue<TValue>(TKey key, TValue value)
+        {
+            var subject = GetRootSubject<TValue>(key);
+
+            if (subject == null)
+            {
+                throw new ArgumentException($"Key {key} does not exist in the configuration store");
+            }
+
+            subject.OnNext(value);
+        }
+
+        protected BehaviorSubject<TValue> GetRootSubject<TValue>(TKey key)
+        {
+            if (!ConfigStore.TryGetValue(key, out var subject))
+            {
+                return null;
+            }
+
+            if (subject is BehaviorSubject<TValue> typedSubject)
+            {
+                return typedSubject;
+            }
+
+            throw new InvalidCastException($"Subject is not of the expected type. Expected <{subject.GetType().GetGenericParameterConstraints().Single().Name}>, got <{typeof(TValue).Name}>");
+        }
+
+        public void Dispose()
+        {
+            Subscriptions?.Dispose();
+        }
+    }
+}
