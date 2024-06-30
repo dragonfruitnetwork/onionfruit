b0db8f680021bc69b7d1d93ef4d852100bfef765
Albie Spriddell <albion@dragonfruit.network>
2024-06-30 20:47:02 +0100

    add dual-state switch control

---
+++ DragonFruit.OnionFruit/Controls/SwitchingControl.cs
@@ -0,0 +1,51 @@
+﻿// OnionFruit Copyright DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under LGPL-3.0. Refer to the LICENCE file for more info
+
+using System.Reactive.Linq;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Presenters;
+using Avalonia.Controls.Templates;
+
+namespace DragonFruit.OnionFruit.Controls
+{
+    public class SwitchingControl : UserControl
+    {
+        private readonly ContentPresenter _presenter;
+
+        public static readonly StyledProperty<bool> SwitchProperty = AvaloniaProperty.Register<SwitchingControl, bool>(nameof(Switch));
+        public static readonly StyledProperty<IDataTemplate> SwitchTrueProperty = AvaloniaProperty.Register<SwitchingControl, IDataTemplate>(nameof(SwitchTrue));
+        public static readonly StyledProperty<IDataTemplate> SwitchFalseProperty = AvaloniaProperty.Register<SwitchingControl, IDataTemplate>(nameof(SwitchFalse));
+
+        public SwitchingControl()
+        {
+            Content = _presenter = new ContentPresenter();
+        }
+
+        protected override void OnInitialized()
+        {
+            var o = this.GetObservable(SwitchProperty).CombineLatest(this.GetObservable(SwitchTrueProperty), this.GetObservable(SwitchFalseProperty));
+
+            _presenter.Bind(ContentPresenter.ContentProperty, this.GetObservable(DataContextProperty));
+            _presenter.Bind(ContentPresenter.ContentTemplateProperty, o.Select(tuple => tuple.Item1 ? tuple.Item2 : tuple.Item3));
+        }
+
+        public bool Switch
+        {
+            get => GetValue(SwitchProperty);
+            set => SetValue(SwitchProperty, value);
+        }
+
+        public IDataTemplate SwitchTrue
+        {
+            get => GetValue(SwitchTrueProperty);
+            set => SetValue(SwitchTrueProperty, value);
+        }
+
+        public IDataTemplate SwitchFalse
+        {
+            get => GetValue(SwitchFalseProperty);
+            set => SetValue(SwitchFalseProperty, value);
+        }
+    }
+}
