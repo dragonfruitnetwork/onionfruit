646126f0ef2b00923ee170a17304634a9d8621ce
Albie Spriddell <albion@dragonfruit.network>
2024-08-25 13:00:35 +0100

    add mainwindow integration

--- DragonFruit.OnionFruit/Views/MainWindow.axaml.cs
+++ DragonFruit.OnionFruit/Views/MainWindow.axaml.cs
@@ -1,10 +1,14 @@
+using System.Reactive;
+using System.Threading.Tasks;
 using Avalonia.Interactivity;
+using DragonFruit.OnionFruit.ViewModels;
 using DragonFruit.OnionFruit.ViewModels.Interfaces;
 using FluentAvalonia.UI.Windowing;
+using ReactiveUI;
 
 namespace DragonFruit.OnionFruit.Views;
 
-public partial class MainWindow : AppWindow
+public partial class MainWindow : ReactiveAppWindow<MainWindowViewModel>
 {
     public MainWindow()
     {
@@ -13,6 +17,14 @@ public partial class MainWindow : AppWindow
         // set titlebar options
         TitleBar.ExtendsContentIntoTitleBar = true;
         TitleBar.TitleBarHitTestType = TitleBarHitTestType.Complex;
+
+        this.WhenActivated(action => action(ViewModel!.SettingsWindowInteraction.RegisterHandler(OpenSettingsWindow)));
+    }
+
+    private async Task OpenSettingsWindow(InteractionContext<Unit, Unit> ctx)
+    {
+        await new SettingsWindow().ShowDialog(this);
+        ctx.SetOutput(Unit.Default);
     }
 
     protected override void OnLoaded(RoutedEventArgs e)
