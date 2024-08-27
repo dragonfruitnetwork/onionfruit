bf0ea1d4779e17677b43eb6e7fc5138078955fa1
Albie Spriddell <albion@dragonfruit.network>
2024-08-27 12:31:53 +0100

    add settings window navigation

--- DragonFruit.OnionFruit/Views/SettingsWindow.axaml.cs
+++ DragonFruit.OnionFruit/Views/SettingsWindow.axaml.cs
@@ -1,19 +1,79 @@
 ﻿// OnionFruit Copyright DragonFruit Network <inbox@dragonfruit.network>
 // Licensed under LGPL-3.0. Refer to the LICENCE file for more info
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Presenters;
+using Avalonia.Controls.Templates;
+using Avalonia.Data;
+using FluentAvalonia.UI.Controls;
 using FluentAvalonia.UI.Windowing;
 
-namespace DragonFruit.OnionFruit.Views
-{
-    public partial class SettingsWindow : AppWindow
-    {
-        public SettingsWindow()
-        {
-            InitializeComponent();
+namespace DragonFruit.OnionFruit.Views;
 
-            // set titlebar options
-            TitleBar.ExtendsContentIntoTitleBar = true;
-            TitleBar.TitleBarHitTestType = TitleBarHitTestType.Complex;
-        }
+// XAML can't have nested classes
+public record SettingsTabInfo(string Name, Symbol Icon, Func<Control> ContentFactory);
+
+public partial class SettingsWindow : AppWindow
+{
+    private static readonly StyledProperty<SettingsTabInfo> SelectedTabProperty = AvaloniaProperty.Register<SettingsWindow, SettingsTabInfo>(nameof(SelectedTab), defaultValue: null, defaultBindingMode: BindingMode.TwoWay);
+
+    public SettingsWindow()
+    {
+        InitializeComponent();
+
+        // set titlebar options
+        TitleBar.ExtendsContentIntoTitleBar = true;
+        TitleBar.TitleBarHitTestType = TitleBarHitTestType.Complex;
+
+        SelectedTab = Tabs.First();
+    }
+
+    public SettingsTabInfo SelectedTab
+    {
+        get => GetValue(SelectedTabProperty);
+        set => SetValue(SelectedTabProperty, value);
+    }
+
+    public IEnumerable<SettingsTabInfo> Tabs { get; } =
+    [
+        new("Countries", Symbol.Globe, () => new ContentPresenter())
+    ];
+
+    public IEnumerable<SettingsTabInfo> FooterTabs { get; } =
+    [
+        new("About OnionFruit", Symbol.Go, () => new ContentPresenter())
+    ];
+}
+
+public class SettingTabViewTemplate : IDataTemplate, IDisposable
+{
+    private IDisposable _lastViewModel;
+
+    public Control Build(object param)
+    {
+        var tabInfo = (SettingsTabInfo)param;
+        var control = tabInfo.ContentFactory.Invoke();
+
+        control.Margin = new Thickness(15);
+        control = new ScrollViewer
+        {
+            Content = control
+        };
+
+        _lastViewModel?.Dispose();
+        _lastViewModel = control.DataContext as IDisposable;
+
+        return control;
+    }
+
+    public bool Match(object data) => data is SettingsTabInfo;
+
+    public void Dispose()
+    {
+        _lastViewModel?.Dispose();
     }
 }
