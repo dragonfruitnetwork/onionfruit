40d0684db098621b6ed58a326fb56cde40134181
Albie Spriddell <albion@dragonfruit.network>
2024-09-19 12:34:55 +0100

    add bridge line list display

--- DragonFruit.OnionFruit/ViewModels/BridgeSettingsTabViewModel.cs
+++ DragonFruit.OnionFruit/ViewModels/BridgeSettingsTabViewModel.cs
@@ -3,11 +3,14 @@
 
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using DragonFruit.OnionFruit.Configuration;
+using DragonFruit.OnionFruit.Core.Config;
 using DragonFruit.OnionFruit.Core.Transports;
 using DragonFruit.OnionFruit.Services;
+using DynamicData;
 using ReactiveUI;
 
 namespace DragonFruit.OnionFruit.ViewModels
@@ -19,17 +22,15 @@ namespace DragonFruit.OnionFruit.ViewModels
 
         private readonly CompositeDisposable _disposables = new();
 
+        private readonly ObservableAsPropertyHelper<bool> _canAddBridgeLines;
         private readonly ObservableAsPropertyHelper<TransportType> _selectedTransport;
+
+        private readonly ReadOnlyObservableCollection<string> _activeTransportBridgeLines;
 
         public BridgeSettingsTabViewModel(OnionFruitSettingsStore settings, TransportManager transports)
         {
             _settings = settings;
             _transports = transports;
-
-            _selectedTransport = settings.GetObservableValue<TransportType>(OnionFruitSetting.SelectedTransportType)
-                .ObserveOn(RxApp.MainThreadScheduler)
-                .ToProperty(this, x => x.SelectedTransport)
-                .DisposeWith(_disposables);
 
             var transportOptions = new Dictionary<TransportType, string>
             {
@@ -42,7 +43,21 @@ namespace DragonFruit.OnionFruit.ViewModels
             }
 
             AvailableTransports = transportOptions;
+
+            var selectedTransport = settings.GetObservableValue<TransportType>(OnionFruitSetting.SelectedTransportType);
+
+            _selectedTransport = selectedTransport.ObserveOn(RxApp.MainThreadScheduler).ToProperty(this, x => x.SelectedTransport).DisposeWith(_disposables);
+            _canAddBridgeLines = selectedTransport.Select(x => x != TransportType.None).ObserveOn(RxApp.MainThreadScheduler).ToProperty(this, x => x.CanAddBridgeLines).DisposeWith(_disposables);
+
+            settings.GetCollection<string>(OnionFruitSetting.UserDefinedBridges)
+                .Connect()
+                .Filter(selectedTransport.Select(BuildBridgeLineFilter))
+                .Bind(out _activeTransportBridgeLines)
+                .Subscribe()
+                .DisposeWith(_disposables);
         }
+
+        public IReadOnlyDictionary<TransportType, string> AvailableTransports { get; }
 
         public TransportType SelectedTransport
         {
@@ -50,7 +65,28 @@ namespace DragonFruit.OnionFruit.ViewModels
             set => _settings.SetValue(OnionFruitSetting.SelectedTransportType, value);
         }
 
-        public IReadOnlyDictionary<TransportType, string> AvailableTransports { get; }
+        public bool CanAddBridgeLines => _canAddBridgeLines.Value;
+        public IEnumerable<string> CurrentBridgeLines => _activeTransportBridgeLines;
+
+        private Func<string, bool> BuildBridgeLineFilter(TransportType type)
+        {
+            if (!_transports.AvailableTransports.TryGetValue(type, out var typeInfo))
+            {
+                return _ => false;
+            }
+
+            return line =>
+            {
+                if (string.IsNullOrEmpty(line))
+                {
+                    return false;
+                }
+
+                // todo cache result?
+                var lineMatch = BridgeEntry.ValidationRegex().Match(line);
+                return lineMatch.Success && lineMatch.Groups["type"].Value.Equals(typeInfo.Id, StringComparison.Ordinal);
+            };
+        }
 
         public void Dispose()
         {
