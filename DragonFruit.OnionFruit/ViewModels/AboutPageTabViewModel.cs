b733827a485082d280c69c4d8f77807d9fa4405f
Albie Spriddell <albion@dragonfruit.network>
2024-09-22 13:16:12 +0100

    add license view to about page

--- DragonFruit.OnionFruit/ViewModels/AboutPageTabViewModel.cs
+++ DragonFruit.OnionFruit/ViewModels/AboutPageTabViewModel.cs
@@ -1,6 +1,10 @@
 ﻿// OnionFruit Copyright DragonFruit Network <inbox@dragonfruit.network>
 // Licensed under LGPL-3.0. Refer to the LICENCE file for more info
 
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using DragonFruit.OnionFruit.Models;
 using FluentAvalonia.UI.Controls;
 using LucideAvalonia.Enum;
 using ReactiveUI;
@@ -9,6 +13,16 @@ namespace DragonFruit.OnionFruit.ViewModels
 {
     public class AboutPageTabViewModel : ReactiveObject
     {
+        public AboutPageTabViewModel()
+        {
+            using var stream = GetType().Assembly.GetManifestResourceStream("DragonFruit.OnionFruit.Assets.nuget-licenses.json");
+            using var readStream = new StreamReader(stream);
+
+            Packages = JsonSerializer.Deserialize<IEnumerable<NugetPackageLicenseInfo>>(readStream.ReadToEnd());
+        }
+
+        public IEnumerable<NugetPackageLicenseInfo> Packages { get; }
+
         public IconSource UpdaterIcon => App.GetIcon(LucideIconNames.RefreshCw);
         public IconSource LicensesIcon => App.GetIcon(LucideIconNames.Scale);
     }
