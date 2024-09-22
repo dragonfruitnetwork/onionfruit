c9cc94a386755d2dad737ceb8d26ded65e4f6f37
Albie Spriddell <albion@dragonfruit.network>
2024-09-22 13:16:00 +0100

    add license exports

---
+++ DragonFruit.OnionFruit/Models/NugetPackageLicenseInfo.cs
@@ -0,0 +1,21 @@
+﻿// OnionFruit Copyright DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under LGPL-3.0. Refer to the LICENCE file for more info
+
+namespace DragonFruit.OnionFruit.Models
+{
+    public record NugetPackageLicenseInfo(
+        string PackageId,
+        string PackageVersion,
+        string PackageProjectUrl,
+        string Copyright,
+        string Authors,
+        string License,
+        string LicenseUrl
+    )
+    {
+        public bool HasLicense => !string.IsNullOrEmpty(License) || !string.IsNullOrEmpty(LicenseUrl);
+        public bool HasProjectUrl => !string.IsNullOrEmpty(PackageProjectUrl);
+
+        public void OpenProjectWebsite() => App.Launch(PackageProjectUrl);
+    }
+}
