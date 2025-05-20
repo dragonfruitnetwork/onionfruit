81356468198fe30fb9f13b83d4e85a32ec5af892
Albie Spriddell <albion@dragonfruit.network>
2025-05-20 20:33:20 +0100

    fix auto restarting

--- DragonFruit.OnionFruit.Windows/Rpc/OnionRpcService.cs
+++ DragonFruit.OnionFruit.Windows/Rpc/OnionRpcService.cs
@@ -2,6 +2,7 @@
 // Licensed under LGPL-3.0. Refer to the LICENCE file for more info
 
 using System.Threading.Tasks;
+using Avalonia.Threading;
 using DragonFruit.OnionFruit.Rpc;
 using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
@@ -15,11 +16,18 @@ namespace DragonFruit.OnionFruit.Windows.Rpc
             // if the old instance launched a new one, we should close
             if (appInstanceManager.ReplacementProcess?.HasExited == false)
             {
-                return Task.FromResult(new SecondInstanceLaunchedResponse
+                try
                 {
-                    ShouldClose = false,
-                    WaitForPidExit = appInstanceManager.CurrentProcess.Id
-                });
+                    return Task.FromResult(new SecondInstanceLaunchedResponse
+                    {
+                        ShouldClose = false,
+                        WaitForPidExit = appInstanceManager.CurrentProcess.Id
+                    });
+                }
+                finally
+                {
+                    Dispatcher.UIThread.Post(() => _ = App.Instance.RequestAppShutdown());
+                }
             }
 
             App.Instance.ActivateApp();
