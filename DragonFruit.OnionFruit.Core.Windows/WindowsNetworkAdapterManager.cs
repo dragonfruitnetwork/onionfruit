0b841bd77ffb07bef9fb27d0da4d36ed67f3e3d8
Albie Spriddell <albion@dragonfruit.network>
2025-05-27 18:23:47 +0100

    replace powershell WMI client with .NET one

--- DragonFruit.OnionFruit.Core.Windows/WindowsNetworkAdapterManager.cs
+++ DragonFruit.OnionFruit.Core.Windows/WindowsNetworkAdapterManager.cs
@@ -4,28 +4,20 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Management;
 using System.Net.Sockets;
 using System.Security;
 using System.Security.AccessControl;
-using System.Threading;
 using DragonFruit.OnionFruit.Core.Network;
-using Microsoft.Management.Infrastructure;
-using Microsoft.Management.Infrastructure.Generic;
-using Microsoft.Management.Infrastructure.Options;
 using Microsoft.Win32;
 
 namespace DragonFruit.OnionFruit.Core.Windows
 {
     public class WindowsNetworkAdapterManager : IDisposable, INetworkAdapterManager
     {
-        private RegistryKey _proxyRegistry, _tcpipConfigRegistry, _tcpip6ConfigRegistry;
-
-        private CimSession _cimSession;
-        private CancellationTokenSource _cimWatcherCancellation;
-        private CimAsyncMultipleResults<CimSubscriptionResult> _cimSessionWatcher;
-
-        private const string CimNamespace = @"root\cimv2";
-        private const string CimNetworkAdapterQuery = "SELECT Description,SettingID FROM Win32_NetworkAdapterConfiguration WHERE IPEnabled = 'TRUE'";
+        private const string WmiNamespace = @"root\cimv2";
+        private const string WmiNetworkAdapterQuery = "SELECT Description,SettingID FROM Win32_NetworkAdapterConfiguration WHERE IPEnabled = 'TRUE'";
+        private const string WmiNetworkAdapterEventQuery = "SELECT * FROM __InstanceCreationEvent WITHIN 2 WHERE TargetInstance ISA 'Win32_NetworkAdapterConfiguration' AND TargetInstance.IPEnabled = TRUE";
 
         private const string DnsRegistryPath = @"SYSTEM\CurrentControlSet\Services\Tcpip{0}\Parameters\Interfaces";
         private const string ProxySettingsPath = @"Software\Microsoft\Windows\CurrentVersion\Internet Settings";
@@ -35,6 +27,9 @@ namespace DragonFruit.OnionFruit.Core.Windows
                                                               | RegistryRights.WriteKey
                                                               | RegistryRights.SetValue
                                                               | RegistryRights.Delete;
+
+        private ManagementEventWatcher _wmiEventWatcher;
+        private RegistryKey _proxyRegistry, _tcpipConfigRegistry, _tcpip6ConfigRegistry;
 
         public NetworkComponentState DnsState => _tcpipConfigRegistry == null && _tcpip6ConfigRegistry == null ? NetworkComponentState.MissingPermissions : NetworkComponentState.Available;
         public NetworkComponentState ProxyState => _proxyRegistry == null ? NetworkComponentState.MissingPermissions : NetworkComponentState.Available;
@@ -60,40 +55,74 @@ namespace DragonFruit.OnionFruit.Core.Windows
             {
             }
 
-            _cimSession = CimSession.Create(null);
-
-            _cimWatcherCancellation = new CancellationTokenSource();
-            _cimSessionWatcher = _cimSession.SubscribeAsync(CimNamespace, "WQL", CimNetworkAdapterQuery, new CimOperationOptions
-            {
-                CancellationToken = _cimWatcherCancellation.Token
-            });
-
-            _cimSessionWatcher.Subscribe(new WimNetworkAdapterSettingsObserver(this));
+            _wmiEventWatcher = new ManagementEventWatcher(WmiNamespace, WmiNetworkAdapterEventQuery);
+            _wmiEventWatcher.EventArrived += WmiEventHandler;
+            _wmiEventWatcher.Start();
         }
 
         public INetworkAdapter GetAdapter(string id)
         {
-            var cimAdapter = _cimSession.QueryInstances(CimNamespace, "WQL", $"{CimNetworkAdapterQuery} AND SettingID = '{id}'").SingleOrDefault();
-            return cimAdapter == null ? null : AdapterFromCimInstance(cimAdapter);
+            using var objectSearcher = new ManagementObjectSearcher(new ManagementScope(WmiNamespace), new WqlObjectQuery($"{WmiNetworkAdapterQuery} AND SettingID = '{id}'"));
+            var adapters = objectSearcher.Get();
+
+            switch (adapters.Count)
+            {
+                case 0:
+                {
+                    return null;
+                }
+
+                case 1:
+                {
+                    var adapter = adapters.Cast<ManagementObject>().SingleOrDefault();
+                    return adapter == null ? null : AdapterFromCimInstance(adapter);
+                }
+
+                default:
+                {
+                    throw new InvalidOperationException($"Multiple adapters found with SettingID '{id}'.");
+                }
+            }
         }
 
         public IList<INetworkAdapter> GetAdapters()
         {
             List<INetworkAdapter> adapters = [new WinGlobalProxyAdapter(_proxyRegistry, false)];
 
-            var results = _cimSession?.QueryInstances(CimNamespace, "WQL", CimNetworkAdapterQuery);
-            if (results != null)
+            using var objectSearcher = new ManagementObjectSearcher(new ManagementScope(WmiNamespace), new WqlObjectQuery(WmiNetworkAdapterQuery));
+            using var results = objectSearcher.Get();
+
+            if (results == null || results.Count == 0)
             {
-                adapters.AddRange(results.Select(AdapterFromCimInstance));
+                return adapters;
             }
 
+            adapters.AddRange(results.Cast<ManagementObject>().Select(AdapterFromCimInstance));
             return adapters;
+        }
+
+        private void WmiEventHandler(object sender, EventArrivedEventArgs e)
+        {
+            if (e.NewEvent == null)
+            {
+                return;
+            }
+
+            using (e.NewEvent)
+            {
+                var newAdapter = (ManagementBaseObject)e.NewEvent["TargetInstance"];
+
+                var id = newAdapter["SettingID"].ToString();
+                var name = newAdapter["Description"].ToString();
+
+                AdapterConnected?.Invoke(this, new NetworkAdapterInfo(id, name, true));
+            }
         }
 
         public void Dispose()
         {
-            _cimWatcherCancellation?.Cancel();
-            _cimSession?.Dispose();
+            _wmiEventWatcher?.Stop();
+            _wmiEventWatcher?.Dispose();
 
             _proxyRegistry?.Dispose();
             _tcpipConfigRegistry?.Dispose();
@@ -102,44 +131,15 @@ namespace DragonFruit.OnionFruit.Core.Windows
             GC.SuppressFinalize(this);
         }
 
-        private WinNetworkAdapter AdapterFromCimInstance(CimInstance instance)
+        private WinNetworkAdapter AdapterFromCimInstance(ManagementObject instance)
         {
-            var id = instance.CimInstanceProperties["SettingID"].Value.ToString();
-            var name = instance.CimInstanceProperties["Description"].Value.ToString();
+            var id = instance["SettingID"].ToString();
+            var name = instance["Description"].ToString();
 
             var tcpipKey = Socket.OSSupportsIPv4 ? _tcpipConfigRegistry?.OpenSubKey(id, RequiredRegistryAccess) : null;
             var tcpip6Key = Socket.OSSupportsIPv6 ? _tcpip6ConfigRegistry?.OpenSubKey(id, RequiredRegistryAccess) : null;
 
             return new WinNetworkAdapter(id, name, !string.IsNullOrEmpty(name), tcpipKey, tcpip6Key);
-        }
-
-        private class WimNetworkAdapterSettingsObserver(WindowsNetworkAdapterManager instance) : IObserver<CimSubscriptionResult>
-        {
-            public void OnCompleted()
-            {
-                // do nothing
-            }
-
-            public void OnError(Exception error)
-            {
-                // do nothing
-            }
-
-            public void OnNext(CimSubscriptionResult value)
-            {
-                using (value)
-                {
-                    if (value.Instance == null)
-                    {
-                        return;
-                    }
-
-                    var id = value.Instance.CimInstanceProperties["SettingID"].Value.ToString();
-                    var name = value.Instance.CimInstanceProperties["Description"].Value.ToString();
-
-                    instance.AdapterConnected?.Invoke(this, new NetworkAdapterInfo(id, name, true));
-                }
-            }
         }
     }
 }
