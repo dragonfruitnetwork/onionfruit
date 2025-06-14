6bf2705a022cea6d55bbee054a8154f54219a70a
Albie Spriddell <albion@dragonfruit.network>
2025-06-14 18:02:38 +0100

    apply small textbox padding changes

--- DragonFruit.OnionFruit/NonWindowsFluentAvaloniaAdjustments.cs
+++ DragonFruit.OnionFruit/NonWindowsFluentAvaloniaAdjustments.cs
@@ -22,11 +22,11 @@ namespace DragonFruit.OnionFruit
 
             Resources["SettingsTabHeaderMargin"] = new Thickness(0, 15, 0, 0);
 
-            Resources["ButtonPadding"] = new Thickness(12, 13, 12, 8);
+            Resources["ButtonPadding"] = new Thickness(12, 15, 12, 8);
             Resources["CheckBoxPadding"] = new Thickness(8, 11, 0, 8);
             Resources["ComboBoxPadding"] = new Thickness(12, 10, 0, 8);
+            Resources["TextControlThemePadding"] = new Thickness(10, 8, 6, 8);
             Resources["ComboBoxItemThemePadding"] = new Thickness(11, 10, 11, 8);
-            Resources["TextControlThemePadding"] = new Thickness(10, 11, 6, 8);
         }
     }
 }
