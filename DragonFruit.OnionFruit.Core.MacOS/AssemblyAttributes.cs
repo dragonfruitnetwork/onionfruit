16057c6bbad08df68de0725815e3336b51a7cd5e
Albie <albion@dragonfruit.network>
2026-06-06 21:03:34 +0100

    add assembly attributes for core projects

---
+++ DragonFruit.OnionFruit.Core.MacOS/AssemblyAttributes.cs
@@ -0,0 +1,6 @@
+// OnionFruit Copyright DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under LGPL-3.0. Refer to the LICENCE file for more info
+
+using System.Runtime.Versioning;
+
+[assembly: SupportedOSPlatform("macos")]
